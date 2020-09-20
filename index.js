a37cb35c6b08819c62734c6e3e300c29b9f0f82b
Josehower <josehowerupn@gmail.com>
2020-09-20 13:57:42 +0200

    Moved require on line 7 11-13

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 const cliProgress = require('cli-progress');
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 bar1.start(100, 0);
- 
-bar1.update(5);
-
-const fs = require("fs")
-
-bar1.update(10);
-
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const download = require('image-downloader')
+const download = require('image-downloader');
+const fs = require('fs');
 
 bar1.update(15);
 
-const text1 = process.argv[2] ? process.argv[2] : undefined
-const text2 = process.argv[3] ? process.argv[3] : "_"
-let compoundText = text1 && text2 ? `${text1}/${text2}` : text1
+const text1 = process.argv[2] ? process.argv[2] : undefined;
+const text2 = process.argv[3] ? process.argv[3] : '_';
+let compoundText = text1 && text2 ? `${text1}/${text2}` : text1;
 
 bar1.update(20);
 
-const dateNow = new Date
+const dateNow = new Date();
 
-fs.mkdir(`./src`, function(err) {
+fs.mkdir(`./src`, function (err) {
   if (err) {
-    if (err.errno === -17){console.log("./src already exist")}
-    else {console.log(err)}
+    if (err.errno === -17) {
+      console.log('./src already exist');
+    } else {
+      console.log(err);
+    }
   } else {
-    console.log("src folder successfully created.")
+    console.log('src folder successfully created.');
   }
-})
+});
 
 bar1.update(30);
 
-fs.mkdir(`./src/images${dateNow}`, function(err) {
+fs.mkdir(`./src/images${dateNow}`, function (err) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log("images folder successfully created.")
+    console.log('images folder successfully created.');
   }
-})
+});
 
 bar1.update(50);
 
-function getImages (html){
-  const $ = cheerio.load(html)
-  const result = Array.from($(".meme-img"))
-  result.length = 10
-  if(text1 === undefined){
-    result.forEach((item, index)=>{downloadImage(item.attribs.src, index, true)})
-  }else{ if(text1 === "clean"){
-     compoundText = undefined;
-     result.forEach((item, index)=>{downloadImage(item.attribs.src.split("/")[1], index)})
-    }
-    else{
-      result.forEach((item, index)=>{downloadImage(item.attribs.src.split("/")[1], index)})
+function getImages(html) {
+  const $ = cheerio.load(html);
+  const result = Array.from($('.meme-img'));
+  result.length = 10;
+  if (text1 === undefined) {
+    result.forEach((item, index) => {
+      downloadImage(item.attribs.src, index, true);
+    });
+  } else {
+    if (text1 === 'clean') {
+      compoundText = undefined;
+      result.forEach((item, index) => {
+        downloadImage(item.attribs.src.split('/')[1], index);
+      });
+    } else {
+      result.forEach((item, index) => {
+        downloadImage(item.attribs.src.split('/')[1], index);
+      });
     }
   }
 }
@@ -62,28 +65,29 @@ function getImages (html){
 bar1.update(70);
 
 fetch('https://memegen.link/examples')
-    .then(res => res.text())
-    .then(body => getImages(body));
+  .then((res) => res.text())
+  .then((body) => getImages(body));
 
+function downloadImage(url, index, clean = false) {
+  const options = clean
+    ? {
+        url: `https://api.memegen.link/images${url}`,
+        dest: `./src/images${dateNow}`, // will be saved to /path/to/dest/image.jpg
+      }
+    : {
+        url: `https://api.memegen.link/images/${url}${
+          compoundText ? '/' + compoundText + '/' + index : ''
+        }.jpg?preview=true&watermark=none`,
+        dest: `./src/images${dateNow}`, // will be saved to /path/to/dest/image.jpg
+      };
 
-function downloadImage (url, index, clean = false) {
-  const options = clean 
-  ? {
-    url: `https://api.memegen.link/images${url}`,
-    dest: `./src/images${dateNow}`               // will be saved to /path/to/dest/image.jpg
-  }
-  : {
-    url: `https://api.memegen.link/images/${url}${compoundText ? "/"+compoundText+"/"+index:""}.jpg?preview=true&watermark=none`,
-    dest: `./src/images${dateNow}`               // will be saved to /path/to/dest/image.jpg
-  } 
-   
-  download.image(options)
+  download
+    .image(options)
     .then(({ filename }) => {
-      console.log('Saved to', filename)  // saved to /path/to/dest/image.jpg
+      console.log('Saved to', filename); // saved to /path/to/dest/image.jpg
     })
-    .catch((err) => console.error(err))
-
+    .catch((err) => console.error(err));
 }
 
-bar1.update(100); 
+bar1.update(100);
 bar1.stop();
